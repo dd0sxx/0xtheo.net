8fe43f274826dea02a40e8125b45b05063618b29
dd0sxx <contacttheotelonis@gmail.com>
2021-11-26 21:38:20 -0500

    added express server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
- 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const path = require('path');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('./index.html');
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(PORT, HOST);
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
