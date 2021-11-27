a88a6ad0f65dc729b4909287ffbcfd4afd3d17ec
dd0sxx <contacttheotelonis@gmail.com>
2021-11-26 19:10:34 -0500

    color rotation

---
+++ index.js
@@ -0,0 +1,30 @@
+let zero, x, theo
+
+zero = document.getElementsByClassName('zero')
+x = document.getElementsByClassName('x')
+theo = document.getElementsByClassName('theo')
+
+let logoClasses = [zero, x, theo]
+let colors = ['black', 'blue', 'white']
+
+let randColor = () => {
+    let ref = [...logoClasses]
+    let col = [...colors]
+    for (let i = 0; i < ref.length; i++) {
+        let rand = Math.floor(Math.random() * ref.length);
+        let rand2 = Math.floor(Math.random() * ref.length);
+        console.log(rand)
+        ref[rand][0].style.color = col[rand2]
+        ref.splice(rand, 1)
+    }
+}
+
+setInterval(() => {
+    randColor()
+    Math.random() > 0.25 ? setTimeout(randColor, 1000) 
+    :
+    Math.random() > 0.5 ? setTimeout(randColor, 5000)
+    : null
+}, 1500)
+
+console.log('hello world')
