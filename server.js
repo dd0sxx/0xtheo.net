3c69fd6201cc7c446d183200fbbc40b4cea30ee9
dd0sxx <contacttheotelonis@gmail.com>
2021-11-26 22:05:16 -0500

    fixed size for logo

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server stated at http://localhost:' + port)
