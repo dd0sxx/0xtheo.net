ddbafaf68e963369afd37d8820b5bd36d00ad80e
dd0sxx <contacttheotelonis@gmail.com>
2021-11-28 13:55:36 -0500

    added 3d shapes

--- public/p5.js
+++ public/p5.js
@@ -8,11 +8,23 @@ function setup() {
   }
   
   function draw() {
-    colorMode(RGB)
+    background(127.5);
+    normalMaterial();
+
+    translate(10, 10);
+    push();
+    rotateZ(frameCount * 0.003);
+    rotateX(frameCount * 0.003);
+    rotateY(frameCount * 0.003);
+    torus(140, 20);
+    pop();
+
+    translate(10, 10);
     push();
     rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
     box(70, 70, 70);
     pop();
+
   }
